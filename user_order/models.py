75464b1d86f6668708f30185c82c7f8aef6a22ae
unknown <wjse0213@naver.com>
2022-07-16 23:14:50 +0900

    build:models

--- user_order/models.py
+++ user_order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    shop_name=models.CharField(max_length=20)
+    shop_address=models.CharField(max_length=40)
+class Order(models.Model):
+    shop=models.ForeignKey(Shop,on_delete=models.CASCADE)
+    order_date= models.DateTimeField("date ordered")
+    address= models.CharField(max_length=40)
+    deliver_finished=models.BooleanField(default=0)
