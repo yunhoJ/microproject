17856be0e3b706cdb6ab4df3b1dd7a3291fe2647
unknown <wjse0213@naver.com>
2022-07-17 15:38:19 +0900

    create api viewset:1.08

--- user_order/views.py
+++ user_order/views.py
@@ -1,33 +1,60 @@
-from django.shortcuts import render
-from requests import request
+from rest_framework.response import Response,status
 from rest_framework import viewsets
-
+from .models import Order,Shop
+from .serializers import ShopSerializers,OrderSerializers
 class ShopViewSets(viewsets.ViewSet):
     def list(self,request):            #/api/shop  
-        pass
+        shops=Shop.objects.all() #shop의 내역을 가져오기
+        serializer=ShopSerializers(shops,many=True)# json형태로 변환 
+        return Response(serializer.data)
 
     def create(self,request):          #/api/shop  
-        pass
-       
+        serializer=ShopSerializers(data= request.data)#포스트맨으로 보내진 json데이터
+        serializer.is_valid(raise_exception=True)#해당 데이터가 유효한지 확인 유효하지 않으면 예외처리 해라  
+        serializer.save()#데이터 저장 
+        return Response(serializer.data,status=status.HTTP_201_CREATE)
+
     def retrieve(self,request,pk=None): #/api/shop/<str:idx>
-        pass
+        shop=Shop.objects.get(id=pk)
+        serializer=ShopSerializers(shop)
+        return Response(serializer.data)
     
     def update(self,request,pk=None):   #/api/shop/<str:idx>
-        pass
+        shop=Shop.objects.get(id=pk)
+        serializer=ShopSerializers(instance=shop,data=request.data)#해당 shop의 데이터를 request.data로 변경해라
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
     def destroy(self,request,pk=None):  #/api/shop/<str:idx>
-        pass
-
+        shop=Shop.objects.get(id=pk)
+        shop.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+         
 class OrderViewSets(viewsets.ViewSet):
     def list(self,request):            #/api/order  
-        pass
+        orders=Order.objects.all()
+        serializer=OrderSerializers(orders,many=True)
+        return Response(serializer.data)
 
     def create(self,request):          #/api/order  
-        pass
-       
+        serializer=OrderSerializers(request.data)      
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATE)
+
     def retrieve(self,request,pk=None): #/api/order/<str:idx>
-        pass
+        order=Order.objects.get(id=pk)
+        serializer=OrderSerializers(order)
+        return Response(serializer.data)
     
     def update(self,request,pk=None):   #/api/order/<str:idx>
-        pass
+        order=Order.objects.get(id=pk)
+        serializer=OrderSerializers(instance=order,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+
     def destroy(self,request,pk=None):  #/api/order/<str:idx>
-        pass
+        order=Order.objects.get(id=pk)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
